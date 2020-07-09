288e3313090b576c2819af025bff5b8d39a9d9d8
EC2 Default User <ec2-user@ip-172-31-17-46.us-east-2.compute.internal>
2020-07-09 23:16:29 +0000

    first commit

---
+++ create.blade.php
@@ -0,0 +1,18 @@
+<!--ここから「PHP/Laravel 11 課題4」より記述200709-->
+
+{{--layouts/profile.blade.phpを読み込む--}}
+@extends('layouts.admin')
+
+{{--profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む--}}
+@section('title', 'プロフィールの新規作成')
+
+{{--admin.blade.phpの@yield('content')に以下のタグを埋め込む--}}
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>Myプロフィール</h2>
+      </div>
+    </div>
+  </div>
+@endsection
